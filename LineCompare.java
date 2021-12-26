21a5bdbe7cc1922f6eb07076fdc042b1e95178df
AkshataHasabi <akshuh818@gmail.com>
2021-12-26 19:07:15 +0530

    ADDED:welcome message

---
+++ LineCompare.java
@@ -0,0 +1,9 @@
+package com.bridgelab.linecomparision;
+
+public class LineCompare {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to line comparision computation");
+	}
+
+}
