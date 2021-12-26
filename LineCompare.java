cc48b9cddae59b0a8574ed1343370022cb5f568c
AkshataHasabi <akshuh818@gmail.com>
2021-12-26 19:15:35 +0530

    ADDED:length of a line

--- LineCompare.java
+++ LineCompare.java
@@ -3,7 +3,13 @@ package com.bridgelab.linecomparision;
 public class LineCompare {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to line comparision computation");
+		int x1, x2, y1, y2;
+		double length;
+		x1=2;
+		x2=4;
+		y1=4;
+		y2=6;
+		length=Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1,  2));
+		System.out.println(length);
 	}
-
 }
